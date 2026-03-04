27034c3543c5754af887f4438dea172ffb21b3fa
Log Lab <testesappandroid@gmail.com>
2026-03-04 18:44:29 +0000

    File changes

--- src/pages/GestorFeedbacks.jsx
+++ src/pages/GestorFeedbacks.jsx
@@ -10,6 +10,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import GestorLayout from "@/components/GestorLayout";
+import { createPageUrl } from "@/utils";
 
 export default function GestorFeedbacks() {
   const [gestor, setGestor] = useState(null);

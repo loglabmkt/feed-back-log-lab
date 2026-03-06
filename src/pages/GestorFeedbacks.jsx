f97683f28130d2755cce97dcb0b6532d7caf4e32
Log Lab <testesappandroid@gmail.com>
2026-03-06 19:01:20 +0000

    File changes

--- src/pages/GestorFeedbacks.jsx
+++ src/pages/GestorFeedbacks.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { Plus } from "lucide-react";
+import { createPageUrl } from "@/utils";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";

9fcbd057053408488fe9a3570528c618a9ad652e
Log Lab <testesappandroid@gmail.com>
2026-03-10 14:53:53 +0000

    File changes

--- src/pages/Feedbacks.jsx
+++ src/pages/Feedbacks.jsx
@@ -300,6 +300,16 @@ export default function Feedbacks() {
                         />
                       </div>
 
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        onClick={() => handleOpenDeadline(template)}
+                        title="Definir prazo"
+                        className="text-amber-600 hover:text-amber-700 hover:bg-amber-50"
+                      >
+                        <CalendarClock className="w-4 h-4" />
+                      </Button>
+
                       <Link to={createPageUrl("EditarFeedback") + `?id=${template.id}`}>
                         <Button variant="outline" size="icon">
                           <Pencil className="w-4 h-4" />
